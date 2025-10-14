6dd2b335897e0401c97203e836df5b6a35aff99a
EmunWebDev <e.felipe.537937@umindanao.edu.ph>
2025-10-14 17:46:10 +0800

    Revert "Add project files."
    This reverts commit 1495e29684e5358c6b5ae475526ac29525568ae7.

--- test/Areas/Admin/Models/IssueBookViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace test.Areas.Admin.Models
-{
-    public class IssueBookViewModel
-    {
-        public int BorrowerId { get; set; }
-        public List<int>? BookIds { get; set; }
-        public DateTime DueDate { get; set; }
-    }
-}
